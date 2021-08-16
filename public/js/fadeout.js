dcef451e65cd268ee83f6a1ba2b5ca378349afa5
jekkiboi <hollymayer4@gmail.com>
2021-08-16 16:23:34 -0700

    homelinks

---
+++ public/js/fadeout.js
@@ -0,0 +1,7 @@
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    setTimeout(() =>{
+        splash.classList.addEventListener('display-none')
+    }, 2000)
+})
